e06d68aa3565dbffae994c4d1b32983944529ed3
yehezkieldio <yehezkieldio35@gmail.com>
2023-08-05 08:05:37 +0800

    refactor: enums

--- database/connect.go
+++ database/connect.go
@@ -15,9 +15,14 @@ func Connect(config config.Configuration) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Makassar", config.DatabaseHost, config.DatabaseUser, config.DatabasePassword, config.DatabaseName, config.DatabasePort)
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	permissionLevel := DB.Exec("DO $$ BEGIN IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'permission_level') THEN CREATE TYPE permission_level AS ENUM ('USER', 'ADMINISTRATOR', 'SUPERADMINISTRATOR'); END IF; END $$;")
+	if permissionLevel.Error != nil {
+		log.Fatal(permissionLevel.Error)
+	}
+
 	log.Println("Database connected")
 }
--- models/user.model.go
+++ models/user.model.go
@@ -1,9 +1,17 @@
 package models
 
+type PermissionLevel string
+
+const (
+	USER               PermissionLevel = "USER"
+	ADMINISTRATION     PermissionLevel = "ADMINISTRATION"
+	SUPERADMINISTRATOR PermissionLevel = "SUPERADMINISTRATOR"
+)
+
 type User struct {
-	ID              uint   `gorm:"primary_key;auto_increment" json:"id"`
-	Username        string `gorm:"size:255;not null;unique" json:"username"`
-	Password        string `gorm:"size:255;not null;" json:"password"`
-	Name            string `gorm:"size:255;not null;" json:"name"`
-	PermissionLevel string `gorm:"size:255;not null;" json:"permission_level"`
+	ID              uint            `gorm:"primary_key;auto_increment" json:"id"`
+	Username        string          `gorm:"size:255;not null;unique" json:"username"`
+	Password        string          `gorm:"size:255;not null;" json:"password"`
+	Name            string          `gorm:"size:255;not null;" json:"name"`
+	PermissionLevel PermissionLevel `gorm:"type:permission_level" json:"permission_level"`
 }
